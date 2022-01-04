42ed06d87f28faaab07d36aff8d13f88f7b323d1
jyc-coder <jhs000123@hanmail.net>
2022-01-04 16:12:50 +0900

    공용 헤더 제작 후 TodoList 스타일링 시작

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { NextPage } from "next";
+import TodoList from "../components/TodoList";
+
+
+
+
+
+
+const app: NextPage = () => {
+    return <TodoList/>
+}
+
+
+
+export default app;
