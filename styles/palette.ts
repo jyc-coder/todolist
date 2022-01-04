cfa683c91d1a3e4593969d4b68640e11893ba455
jyc-coder <jhs000123@hanmail.net>
2022-01-04 15:52:00 +0900

    색상 단축키 파일 생성

---
+++ styles/palette.ts
@@ -0,0 +1,11 @@
+export default {
+    red: "#FFAFBO",
+    orange: "#FFC282",
+    yellow: "#FCFFB0",
+    green: "#E2FFAF",
+    blue: "#AEE4FF",
+    navy: "#B5C7ED",
+    gray: "#E5E5E5",
+    deep_red: "#F3456",
+    deep_green: "#47E774",
+};
