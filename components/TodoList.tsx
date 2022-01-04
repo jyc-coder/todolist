af4c77fba73c37756732718c721c2b594055953a
jyc-coder <jhs000123@hanmail.net>
2022-01-04 17:21:55 +0900

    색깔 객체별 남을 할일 갯수 표현

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -6,18 +6,62 @@ import { useMemo } from "react";
 
 const Container = styled.div`
     width: 100%;
-
-    .todo-list-header{
+    /* 헤더 */ 
+    .todo-num{
+        margin-left: 12px;
+    }
+    .todo-list-header{ 
         padding: 12px;
+        position: relative;
         border-bottom: 1px solid ${palette.gray};
 
         .todo-list-last-todo {
             font-size: 14px;
+            margin: 0 0 8px;
             span {
-                margin-left: 8px;
+                margin-left: 8px;               
             }
         }
-    }
+            .todo-list-header-colors {
+                display:flex;
+                /* 남은 할일의 수를 색깔별로 표시할 숫자 */
+                .todo-list-header-color-num {
+                    display:flex;
+                    margin-right:8px;
+                    p{
+                        font-size: 14px;
+                        line-height:16px;
+                        margin: 0;
+                        margin-left: 6px;
+                    }
+                    /* 그 숫자 옆에 표시되는 동그라미 */
+                    .todo-list-header-round-color{
+                        width:16px;
+                        height: 16px;
+                        border-radius:50%
+                    }
+                }
+            }
+        }
+        /* 색깔별 단축키 표시 */
+        .bg-blue{
+            background-color: ${palette.blue};
+        }
+        .bg-green {
+            background-color: ${palette.green};
+        }
+        .bg-navy {
+            background-color: ${palette.navy};
+        }
+        .bg-orange {
+            background-color: ${palette.orange};
+        }
+        .bg-red {
+            background-color: ${palette.red};
+        }
+        .bg-yellow {
+            background-color: ${palette.yellow};
+        }    
 `;
 
 //* TodoList가 props로 todos를 받을 수 있게 타입을 설정
@@ -51,6 +95,14 @@ const TodoList: React.FC<IProps> = ({todos}) => {
                 <p className="todo-list-last-todo">
                     남은 TODO<span>{todos.length}개</span>
                 </p>
+                <div className="todo-list-header-colors">
+                    {Object.keys(todoColorNums).map((color, index) => (
+                        <div className="todo-list-header-color-num" key={index}>
+                            <div className={`todo-list-header-round-color bg-${color}`}></div>
+                            <p>{todoColorNums[color]}개</p>
+                        </div>
+                    ))}
+                </div>
             </div>
         </Container>
     );
