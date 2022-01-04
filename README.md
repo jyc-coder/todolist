5938146f56ea4d25f962aa5601684980be7074ab
jyc-coder <jhs000123@hanmail.net>
2022-01-04 15:47:45 +0900

    투두 리스트 헤더 개발 시작

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 할 일을 적어서 추가하여 볼 수 있는 to-do list
 
 1. 패키지 생성 및 넥스트 설치
+
+2. 투두 리스트 헤더 개발 시작 components/Header.tsx
--- pages/indes.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { NextPage } from "next";
-
-const index : NextPage = () => {
-    return <div>Hello </div>;
-};
-
-export default index;
