d8881482446d8d303daf735bc8a9f212963a7298
jyc-coder <jhs000123@hanmail.net>
2022-01-04 20:10:20 +0900

    5시간 초과로 코딩 종료

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,81 +1,81 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "plugin:react/recommended",
-        "airbnb"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 13,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    rules: {
-        quotes : ["error" , "double"],
-        "linebreak-style": ["error", "windows"],
-        "space-infix-ops" : ["error" , {"int32Hint" : false }],
-        "spaced-comment": ["error", "always", { "exceptions": ["-", "+"] }],
-        "jsx-a11y/label-has-associated-control": ["error", {
-          "required": {
-            "some": ["nesting", "id"]
-          }
-        }],
-        "jsx-a11y/label-has-for": ["error", {
-          "required": {
-            "some": ["nesting", "id"]
-          }
-        }],
-        "arrow-body-style": "off", // 화살표 함수 안에 return을 사용할 수 있다.
-        "react/no-array-index-key" : "off", //key값으로 index를 사용할 수 있다.
-        "comma-dangle": "off", // 마지막에 ,을 넣어주지 않는다.
-        "react/no-unescaped-entities" : "off", //문자열 내에서 " ' > } 허용
-        "react/prop-types" : "off", //proptypes를 사용하지않는다.
-        "object-curly-newline" : "off", // { 다음 줄 바꿈을 강제로 사용하지 않는다.
-        "react/jsx-one-expression-per-line" : "off" , //한 라인에 여러 개의 jsx를 사용할 수 있다.
-        "implicit-arrow-linebreak" : "off" , //화살표 함수 다음에 줄 바꿈을 사용할 수 있다.
-        "no-shadow":"off", //파일 내에서 중복 이름을 사용할 수 있다.
-        "operator-line-break" : "off", //연산자 다음 줄 바꿈을 사용할 수 있다.
-        "react/react-in-jsx-scope" : "off", //jsx를 사용하여도 React를 꼭 import하지 않아도 된다.
-        "react/jsx-props-no-spreading": "off" , //props를 스프레드 할 수 있다.
-        "jsx-a11y/anchor-is-valid" : "off" , //next js에서는 a에 href없이 사용 
-        "global-require" : "off" , //함수 내에서 require 사용 가능
-        "no-use-before-define": "off" , //선언전에 사용하지 말라,
-        "import/prefer-default-export" : "off" , //export default 권장
-        "no-param-reassign" : "off" , //param assign 하지 않기
-        "jsx-a11y/label-has-associated-control" : "off",
-        "no-invalid-css": "off",
-        "no-confusing-arrow" : "off",
-        "react/jsx-curly-newline" : "off",
-        indent : "off",
-        "react/jsx-filename-extension": [
-          1,
-          { extenstions: [".js", ".jsx", ".tsx"]}, //jsx사용 가능한 확장자 설정
-        ],
-        "import/extensions" : [
-          "error",
-          "ignorePackages",
-          {
-            js:"never",
-            jsx:"never",
-            ts: "never",
-            tsx: "never", 
-          }, //import 시 확장자명은 사용하지 않는다.
-        ],
-    },
-    settings:{
-      "import/resolver" : {
-        node: {
-          extensions: [".js", ".jsx", ".ts", ".tsx", ".d.ts"],
-        },
+  "env": {
+      "browser": true,
+      "es2021": true
+  },
+  "extends": [
+      "plugin:react/recommended",
+      "airbnb"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+      "ecmaFeatures": {
+          "jsx": true
+      },
+      "ecmaVersion": 13,
+      "sourceType": "module"
+  },
+  "plugins": [
+      "react",
+      "@typescript-eslint"
+  ],
+  rules: {
+      quotes : ["error" , "double"],
+      "linebreak-style": ["error", "windows"],
+      "space-infix-ops" : ["error" , {"int32Hint" : false }],
+      "spaced-comment": ["error", "always", { "exceptions": ["-", "+"] }],
+      "jsx-a11y/label-has-associated-control": ["error", {
+        "required": {
+          "some": ["nesting", "id"]
+        }
+      }],
+      "jsx-a11y/label-has-for": ["error", {
+        "required": {
+          "some": ["nesting", "id"]
+        }
+      }],
+      "arrow-body-style": "off", // 화살표 함수 안에 return을 사용할 수 있다.
+      "react/no-array-index-key" : "off", //key값으로 index를 사용할 수 있다.
+      "comma-dangle": "off", // 마지막에 ,을 넣어주지 않는다.
+      "react/no-unescaped-entities" : "off", //문자열 내에서 " ' > } 허용
+      "react/prop-types" : "off", //proptypes를 사용하지않는다.
+      "object-curly-newline" : "off", // { 다음 줄 바꿈을 강제로 사용하지 않는다.
+      "react/jsx-one-expression-per-line" : "off" , //한 라인에 여러 개의 jsx를 사용할 수 있다.
+      "implicit-arrow-linebreak" : "off" , //화살표 함수 다음에 줄 바꿈을 사용할 수 있다.
+      "no-shadow":"off", //파일 내에서 중복 이름을 사용할 수 있다.
+      "operator-line-break" : "off", //연산자 다음 줄 바꿈을 사용할 수 있다.
+      "react/react-in-jsx-scope" : "off", //jsx를 사용하여도 React를 꼭 import하지 않아도 된다.
+      "react/jsx-props-no-spreading": "off" , //props를 스프레드 할 수 있다.
+      "jsx-a11y/anchor-is-valid" : "off" , //next js에서는 a에 href없이 사용 
+      "global-require" : "off" , //함수 내에서 require 사용 가능
+      "no-use-before-define": "off" , //선언전에 사용하지 말라,
+      "import/prefer-default-export" : "off" , //export default 권장
+      "no-param-reassign" : "off" , //param assign 하지 않기
+      "jsx-a11y/label-has-associated-control" : "off",
+      "no-invalid-css": "off",
+      "no-confusing-arrow" : "off",
+      "react/jsx-curly-newline" : "off",
+      indent : "off",
+      "react/jsx-filename-extension": [
+        1,
+        { extenstions: [".js", ".jsx", ".tsx" ]}, //jsx사용 가능한 확장자 설정
+      ],
+      "import/extensions" : [
+        "error",
+        "ignorePackages",
+        {
+          js:"never",
+          jsx:"never",
+          ts: "never",
+          tsx: "never", 
+        }, //import 시 확장자명은 사용하지 않는다.
+      ],
+  },
+  settings:{
+    "import/resolver" : {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".d.ts"],
       },
     },
+  },
 };
